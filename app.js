e5f0b3c94655a326dff771488fac9688a81eacdd
Hussain-Aqeel <h.aljassim.1999@gmail.com>
2021-03-05 20:15:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+
+
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults , 2000);
+  e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults() {
+  const amount = document.getElementById('amount');
+  const intrest = document.getElementById('intrest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalIntrests = document.getElementById('total-intrests');
+
+  const principal = parseFloat(amount.value);
+  const calculatedIntrest = parseFloat(intrest.value) / 100 / 12 ;
+  const calculatedPayments = parseFloat(years.value) * 12 ;
+
+  // Compute monthly payment
+
+  const x = Math.pow(1 + calculatedIntrest, calculatedPayments);
+  const monthly = (principal * x * calculatedIntrest)/ (x - 1);
+
+  if (isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalIntrests.value = ((monthly * calculatedPayments) - principal).toFixed(2); 
+
+    document.getElementById('results').style.display = 'block';
+
+    document.getElementById('loading').style.display = 'none';
+  }
+  else {
+    showError('please check your numbers..');
+  }
+
+}
+
+function showError(error) {
+  // create div
+  const errorDiv = document.createElement('div');
+
+  // get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Add text to element
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // clear error after certain amount of time
+  setTimeout(clearError, 3000);
+
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+}
+
+function clearError(){
+  document.querySelector('.alert').remove();
+}
+
+
+
+
+
